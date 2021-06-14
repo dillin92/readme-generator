e0b4c043d48c4056fc35b06093304fe627b90ad8
dillin92 <dillin970@gmail.com>
2021-06-13 23:54:40 -0400

    what I have so far

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [
-    question = "What is your project title?"];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-    .prompt([
-       question= questions.question
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: (value) => {if(value){return true} else {return "Please enter a value to continue"}},
+    },
+     {
+         type: 'input',
+         name: 'installation',
+         message: 'How do you install your application?',
+         validate: (value) => {if(value){return true} else {return "Please enter a value to continue"}}
+    }
     ])
-    .then((answers) => {
-
+    .then((data) => {
+        
+        console.log(data);
     })
     .catch((error) => {
-        if (error.isTtyError) {
-            console.log(err);
+        if (error) {
+            console.log(error);
         } else {
             console.log("something else went wrong!");
         }
-    });
-};
+});
 
-// Function call to initialize app
-init();
+// TODO: Create a function to write README file
+function writeToFile (data) {
+   return new Promise((resolve, reject) => {
+
+        fs.writeFile('./dist/new-README.md', data, function(err){
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Markdown File Created'
+            });
+            console.log('Success!');
+        });
+    });
+}
+
+    
+// }
+// // TODO: Create a function to initialize app
+// function init() {
+  
+ 
+// };
+
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -11,10 +12,31 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// TODO: Create a function to write README file
+function writeToFile (data) {
+  return new Promise((resolve, reject) => {
+
+       fs.writeFile('./dist/new-README.md', data, function(err){
+           if (err) {
+               reject(err);
+               return;
+           }
+           resolve({
+               ok: true,
+               message: 'Markdown File Created'
+           });
+           console.log('Success!');
+       });
+   });
+};
+
 function generateMarkdown(data) {
+  writeToFile();
   return `# ${data.title}
+  *Installation
+  ${data.installation}
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown, writeToFile};
